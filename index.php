37542d4af827aa9f0a10095f2d2d5a034928a877
Muhammad Arief <93487560+MuhammadAriefTI20@users.noreply.github.com>
2024-02-19 02:45:49 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,145 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>:: T0UCH3D BY L0N3LY W4RR10R ::</title>
+    <meta name="Numpang Galau Yak :v"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400..800;1,400..800&family=Protest+Revolution&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+<style>
+ body,html {
+   margin: 0;
+   padding: 0;}
+</style>
+  <body style="background-color: black;" >
+    <div class="modal position-static d-block" tabindex="-1">
+      <div class="modal-dialog">
+        <div class="modal-content bg-body-tertiary">
+          <div class="modal-header">
+          </div>
+          <div class="modal-body text-center">            
+            <h5  id="te2" class="text-center" style="font-family: Protest Revolution, sans-serif;font-weight: 400;  font-style: normal;"><b>!!! My Sadness !!!</b></h5>
+            <h5 id="te1" class="text-center" style="font-family: Protest Revolution, sans-serif;font-weight: 400;  font-style: normal; display: none;"><b>!!! L0N3LY W4RR10R Wuzz Here !!!</b></h5>
+           <center><img id="gm2" class="img-fluid" style="width: 200px;display: none;" src="https://i.ibb.co/nMTZSTQ/gambarlogo.png"/>
+             <img id="gm"  class="img-fluid" style="width: 200px; " src="https://i.ibb.co/mC3D1bG/vecteezy-ai-generative-black-rose-flower-png-24396765.png"/>
+<br>     <audio id="audio2" controls style="display: none;">
+          <source  src="https://giftmooproject.com/lilwill-mydoughi.mp3" type="audio/mpeg">
+          </audio>
+          <audio id="audio" controls style="display: none;">
+          <source  src="https://giftmooproject.com/sadness&sorrow.mp3" type="audio/mpeg">
+          </audio></center>
+ <br>          <button type="button" class="btn btn-primary" onclick="typeWriter()" id="satu">Click Me ^_^</button>
+            <br>
+            <h6 id="demos" style="margin-left: 20px; margin-left: 20px;"></h6>
+
+          </div>
+          <div id="divone" style="display: none;"><hr>
+          <p class="text-center" style="font-family: EB Garamond, serif; font-size: 20px"><font style="color: red;">::</font> Greetingz Old Friend <font style="color: red;">::</font></p><p class="text-center" style="font-family: EB Garamond, serif;">
+            Crusher - Mr.Explor5 - WannabeID - Pineapple_303 <br>Terkejoed_12 -
+Mr.Xid - Denstroke <br> TU4N N1CKN4M3 - PomPom
+<br><br>since.2k18</font> <br> <font size='1' style="color: red;" font face='courier new' >&copy;destroysquad.com</font>
+          </p></div>
+          <div class="modal-footer">
+          </div>
+        </div>
+      </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script>
+    var txt = "Hey Cantik... \n Terimakasih ya karna pernah menerimaku.^_^ \n Terimakasih juga karna udah nemenin aku menjalani 3 tahun yang indah ini..  \n mungkin kamu memang bukan milikku, aku masih kurang pantas untuk mendapatkanmu, sekarang aku mengerti. namun perlu kamu ketahui aku masih akan selalu mau menjaga dan membantumu... \n jangan sungkan untuk mengatakan apapun itu.. akan aku lakukan selagi aku mampu, aku bukan bermaksud untuk kembali namun ada janji yang harus aku tepati... tetap semangat ya cantik,, aku senang karna kamu pernah menjadi bagian dari perjuanganku... kini aku akan berjuang sendiri... ^_^  \n";
+    var speed = 130;
+    var i = 0;
+    var txt2 = "\n eh... Kok jadi menyedihkan gini ya !? haha... gak bisa dibiarkan nih !! Ganti Text !! Munculkan Old Member !! ! Attack ! ";
+    var speed2 = 130;
+    var b=0;
+    var txt3 = "! W4r Mode !";
+    var speed3 = 120;
+    var c=0;
+    const btn = document.getElementById('satu');
+    var myAudio = document.getElementById('audio');
+    var myAudio2 = document.getElementById('audio2');
+    var gm = document.getElementById('gm2');
+    var gm2 = document.getElementById('gm');
+    var te1 = document.getElementById('te1');
+    var te2 = document.getElementById('te2');
+    var dipw = document.getElementById('divone');
+
+    var elem = document.documentElement;
+    function openFullscreen() {
+      if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+      } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen();
+      } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen();
+      }
+    }
+    function typeWriter() {
+    openFullscreen();
+    myAudio.play();
+    myAudio.style.display = 'block';
+    if (i < txt.length) {
+        document.getElementById("demos").innerHTML += txt.charAt(i);
+        i++;
+        btn.style.display = 'none';
+
+        setTimeout(typeWriter, speed);
+        return i;
+    }
+    k = txt.length;
+    if (k == i) {
+      document.getElementById("demos").innerHTML = "";
+      typeWriter2();
+    }
+    } 
+function typeWriter2() {
+	if (b < txt2.length) {
+    document.getElementById("demos").innerHTML += txt2.charAt(b);
+    b++;
+    setTimeout(typeWriter2, speed2);
+    if (b == 71) {
+  document.getElementById("demos").innerHTML = "";
+  }if (b == 88) {      
+    te2.style.display = 'none';
+    te1.style.display = 'block';
+  document.getElementById("demos").innerHTML = "";
+  }
+  if (b == 111) {      
+    dipw.style.display = 'block';
+  document.getElementById("demos").innerHTML = "";
+  }
+	return b;
+  }
+if (b>111) {
+  document.getElementById("demos").innerHTML = "";
+
+  typeWriter3(); }}
+function typeWriter3() { 
+	if (c < txt3.length) {
+    document.getElementById("demos").innerHTML += txt3.charAt(c);
+    c++;
+    setTimeout(typeWriter3, speed3);
+	return c;
+  }
+if (c>5) {
+  gm2.style.display = 'none';
+      gm.style.display = 'block';
+
+      dipw.style.display = 'block';
+      document.getElementById("demos").style.color = "red";
+      document.getElementById("demos").style.fontFamily = "Protest Revolution, sans-serif";
+      document.getElementById("demos").style.fontSize = "25px";
+      myAudio.pause();
+      myAudio.style.display = 'none'; 
+      myAudio2.play();
+      myAudio2.style.display = 'block';
+
+}
+}
+      </script>
+  </body>
+</html>
